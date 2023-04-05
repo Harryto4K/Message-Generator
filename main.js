e7dedb694c76946c44cba3a0cd361dab8ca9a8e1
Harryto4K <harryyadier@gmail.com>
2023-04-04 23:39:34 -0400

    added time

--- main.js
+++ main.js
@@ -9,10 +9,15 @@ function random() {
         'normal'
     ]
 
+    const time = [
+        'day',
+        'night'
+    ]
+
     //variable that contains the messages
     let message = [
-        `You will have a ${words[Math.floor(Math.random() * words.length)]} day`,
-        `You will not have a ${words[Math.floor(Math.random() * words.length)]} day`
+        `You will have a ${words[Math.floor(Math.random() * words.length)]} ${time[Math.floor(Math.random() * time.length)]}`,
+        `You will not have a ${words[Math.floor(Math.random() * words.length)]} ${time[Math.floor(Math.random() * time.length)]}`
     ]
 
     return message[Math.floor(Math.random() * message.length)]; 
